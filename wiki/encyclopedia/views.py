9fb9fd5acef1f8969ba0f8b3beca03bee0a36419
HeroHern <69167141+HeroHern@users.noreply.github.com>
2021-08-27 00:00:01 -0700

    added entry view

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +9,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    content = util.get_entry(entry)
+    if content is None:
+        raise Http404("Entry does not exist")
+    
+    return render(request, "encyclopedia/entry_page.html", {
+        "content" : content
+        ## convert  md
+    })
